2823d0bd835bcae33b25fcc9c980b7d9c267a6ae
satish486 <51025174+satish486@users.noreply.github.com>
2021-09-24 19:06:33 +0530

    Add files via upload

---
+++ sourcefile.ps1
@@ -0,0 +1,20 @@
+﻿Clear-Host
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+$ArtifactStorageLocation="C:\artifacts\artifacts-storage\"
+$TomcatDestination="C:\Program Files\Apache Software Foundation\Tomcat 9.0\webapps\"
+cd "C:\artifacts\artifacts-storage"
+$Foldername= $(get-date -f MM-dd-yyyy_HH_mm_ss)
+$ArtifactDestination=$ArtifactStorageLocation+$Foldername
+mkdir $Foldername
+Write-Host "********Copying the WAR file to Artifactory location*************"
+Copy-Item "C:\Program Files (x86)\Jenkins\workspace\CI-CD-Pipeline-Tomcat\target\java-tomcat-maven-example.war" -Destination $ArtifactDestination
+Write-Host "********WAR Copied to Artifactory location**********"
+Write-Host "Stopping the Web server"
+Stop-Service -Name "Tomcat9"
+Write-Host "******Deploying the WAR to Web server***********"
+Copy-Item $ArtifactDestination\java-tomcat-maven-example.war -Destination $TomcatDestination
+write Start-Sleep 5
+Start-Service -Name "Tomcat9"
+Write-Host "*****Deployment completed and services started******"
+
+
